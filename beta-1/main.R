12d3d19035ffa8d0a7b2ef246babb6f8180f5a9a
tristantao <tao.tristan@gmail.com>
2014-05-14 17:25:06 -0700

    commenting out the output code

--- beta-1/main.R
+++ beta-1/main.R
@@ -26,5 +26,10 @@ sample_cs_a_responseData = cs_a_responseData[sample(nrow(cs_a_responseData), 30)
 sample_non_cs_stem_a_response_data = non_cs_stem_a_response_data[sample(nrow(non_cs_stem_a_response_data), 30), ]
 samlpe_non_stem_a_response_data = non_stem_a_response_data[sample(nrow(non_stem_a_response_data), 30), ]
 
+#Already ran, don't run it again.
+#write.csv(sample_cs_a_responseData$Email, file = "beta-1.0-cs.csv", row.names = FALSE)
+#write.csv(sample_non_cs_stem_a_response_data$Email, file = "beta-1.0-non-cs-stem.csv", row.names = FALSE)
+#write.csv(samlpe_non_stem_a_response_data$Email, file = "beta-1.0-non-stem.csv", row.names = FALSE)
+
 
 
