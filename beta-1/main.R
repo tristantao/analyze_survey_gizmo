9285e1dd75840d08a2ef47f3762e7c456c3421b5
tristantao <tao.tristan@gmail.com>
2014-05-19 14:53:05 -0700

    access code chronological, no longer random sample

--- beta-1/main.R
+++ beta-1/main.R
@@ -5,26 +5,46 @@ names(responseData)
 
 responseData =  responseData[,names(responseData) == "major_cluster"
                              | names(responseData) == "interested" 
-                            | names(responseData) == "Email"]
+                             | names(responseData) == "Email"
+                             | names(responseData) == "Date.Submitted"]
 
 dim(responseData)
 
 approachable_responseData = responseData[responseData$"interested" 
-                                        == TRUE,]
+                                         == TRUE,]
 
-cs_a_responseData = approachable_responseData[approachable_responseData$"major_cluster" == "cs", ]
+sent_cs = read.csv("bucketed_users/beta-1.0-cs.csv", header = TRUE, stringsAsFactors = FALSE)
+sent_non_cs = read.csv("bucketed_users/beta-1.0-non-cs-stem.csv", header = TRUE, stringsAsFactors = FALSE)
+sent_non_stem_cs = read.csv("bucketed_users/beta-1.0-non-stem.csv", header = TRUE, stringsAsFactors = FALSE)
+dim(sent_cs)
+dim(sent_non_cs)
+dim(sent_non_stem_cs)
+
+cs_a_responseData = approachable_responseData[approachable_responseData$"major_cluster" == "cs" &
+                                                !(approachable_responseData$"Email" %in% sent_cs$Email), ]
 dim(cs_a_responseData)
 
-non_cs_stem_a_response_data = approachable_responseData[approachable_responseData$"major_cluster" == "non-cs-stem", ]
+non_cs_stem_a_response_data = approachable_responseData[approachable_responseData$"major_cluster" == "non-cs-stem" &
+                                                        !(approachable_responseData$"Email" %in% sent_non_cs$Email), ]
 dim(non_cs_stem_a_response_data)
 
-non_stem_a_response_data = approachable_responseData[approachable_responseData$"major_cluster" == "non-stem", ]
+non_stem_a_response_data = approachable_responseData[approachable_responseData$"major_cluster" == "non-stem" &
+                                                     !(approachable_responseData$"Email" %in% sent_non_stem_cs$Email), ]
 dim(non_stem_a_response_data)
 
-#Now grab the random sampled 30 people.
-sample_cs_a_responseData = cs_a_responseData[sample(nrow(cs_a_responseData), 30), ]
-sample_non_cs_stem_a_response_data = non_cs_stem_a_response_data[sample(nrow(non_cs_stem_a_response_data), 30), ]
-samlpe_non_stem_a_response_data = non_stem_a_response_data[sample(nrow(non_stem_a_response_data), 30), ]
+new_batch_cs = cs_a_responseData[1:30,]
+new_batch_non_cs_stem = non_cs_stem_a_response_data[1:30,]
+new_batch_non_stem  = non_stem_a_response_data[1:30,]
+
+write.csv(new_batch_cs$Email, file="batch-cs.csv", row.names = FALSE,)
+write.csv(new_batch_non_cs_stem$Email, file="batch-non-cs-stem.csv", row.names = FALSE)
+write.csv(new_batch_non_stem$Email, file="batch-non-stem.csv", row.names = FALSE)
+
+
+#Grab the random sampled 30 people.
+#sample_cs_a_responseData = cs_a_responseData[sample(nrow(cs_a_responseData), 30), ]
+#sample_non_cs_stem_a_response_data = non_cs_stem_a_response_data[sample(nrow(non_cs_stem_a_response_data), 30), ]
+#samlpe_non_stem_a_response_data = non_stem_a_response_data[sample(nrow(non_stem_a_response_data), 30), ]
 
 #Already ran, don't run it again.
 #write.csv(sample_cs_a_responseData$Email, file = "beta-1.0-cs.csv", row.names = FALSE)
