35f21c2dffe99698e6e95a0bb95fc332d63c138e
tristantao <tao.tristan@gmail.com>
2014-06-10 11:52:17 -0700

    export now uses excess and favors cs batch

--- beta-1/main.R
+++ beta-1/main.R
@@ -1,6 +1,6 @@
 setwd('/Users/t-rex-Box/Desktop/work/analyze_survey_gizmo/beta-1')
 
-responseData <- read.csv("20140514181801-SurveyExport.csv", header = TRUE, stringsAsFactors = FALSE)
+responseData <- read.csv("20140603145109-SurveyExport.csv", header = TRUE, stringsAsFactors = FALSE)
 names(responseData)
 
 responseData =  responseData[,names(responseData) == "major_cluster"
@@ -16,9 +16,15 @@ approachable_responseData = responseData[responseData$"interested"
 sent_cs = read.csv("bucketed_users/beta-1.0-cs.csv", header = TRUE, stringsAsFactors = FALSE)
 sent_non_cs = read.csv("bucketed_users/beta-1.0-non-cs-stem.csv", header = TRUE, stringsAsFactors = FALSE)
 sent_non_stem_cs = read.csv("bucketed_users/beta-1.0-non-stem.csv", header = TRUE, stringsAsFactors = FALSE)
+excess = read.csv("bucketed_users/beta-1.0-excess.csv", header = TRUE, stringsAsFactors = FALSE)
+  
 dim(sent_cs)
 dim(sent_non_cs)
 dim(sent_non_stem_cs)
+dim(excess)
+
+#Remove the excess category emails
+approachable_responseData = approachable_responseData[!(approachable_responseData$"Email" %in% excess$Email),]
 
 cs_a_responseData = approachable_responseData[approachable_responseData$"major_cluster" == "cs" &
                                                 !(approachable_responseData$"Email" %in% sent_cs$Email), ]
@@ -32,7 +38,7 @@ non_stem_a_response_data = approachable_responseData[approachable_responseData$"
                                                      !(approachable_responseData$"Email" %in% sent_non_stem_cs$Email), ]
 dim(non_stem_a_response_data)
 
-new_batch_cs = cs_a_responseData[1:60,]
+new_batch_cs = cs_a_responseData[1:180,]
 new_batch_non_cs_stem = non_cs_stem_a_response_data[1:60,]
 new_batch_non_stem  = non_stem_a_response_data[1:60,]
 
